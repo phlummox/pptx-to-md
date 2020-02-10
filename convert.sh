6bd16c9cdf28946cd0ab9b8766b6eea1410de705
phlummox <phlummox@localhost>
2020-02-10 13:33:47 +0000

    fix incorrect bash test flag

--- convert.sh
+++ convert.sh
@@ -9,7 +9,7 @@ for file in $args; do
   ../pptx-to-yaml.py $file ${b}.yaml ${b}_figures
   ../yaml-to-md.py ${b}.yaml ${b}.md
   for svg_file in ${b}_figures/*.svg; do
-    if [ -x $svg_file ] ; then
+    if [ -a $svg_file ] ; then
       svg_base=`basename $svg_file .svg`
       inkscape --export-eps=${b}_figures/${svg_base}.eps $svg_file
     fi
